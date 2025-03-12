63ddf3a2f60c39e44af0a76a7b2769d8804305c0
martinsandoval <jsandovala001@uaemex.mx>
2025-03-12 16:22:03 -0600

    Added functionality to select and delete multiple files from the file list area

--- print.py
+++ print.py
@@ -28,13 +28,22 @@ def open_files():
             pyautogui.hotkey('ctrl', 'p')
             time.sleep(2)
 
-            print('Here the Enter key should be pressed to print the file')
             pyautogui.press('enter')
             time.sleep(2)
         except Exception as e:
             print(f'Error opening file {file}: {e}')
     
     file_list.delete(0, tkinter.END)
+
+def remove_selected_files():
+    selected_files = file_list.curselection()
+    for index in reversed(selected_files):
+        file_list.delete(index)
+
+def update_selected_count(event=None):
+    selected_files = file_list.curselection()
+    number_files_selected = len(selected_files)
+    remove_button.config(text=f'Borrar ({number_files_selected}) seleccionados')
 
 # Window configuration
 root = TkinterDnD.Tk()
@@ -51,9 +60,14 @@ select_button.pack(side=tkinter.LEFT, padx=10)
 open_button = tkinter.Button(frame_buttons, text="Imprimir Archivos", command=open_files, width=20)
 open_button.pack(side=tkinter.RIGHT, padx=10)
 
+remove_button = tkinter.Button(frame_buttons, text="Borrar (0) seleccionados", command=remove_selected_files, width=20)
+remove_button.pack(side=tkinter.LEFT, padx=10)
+
 # List where the files are displayed
-file_list = tkinter.Listbox(root, width=80, height=15)
+file_list = tkinter.Listbox(root, width=80, height=15, selectmode=tkinter.MULTIPLE)
 file_list.pack(expand=True, fill="both", padx=10, pady=10)
+
+file_list.bind('<<ListboxSelect>>', update_selected_count)
 
 # Enable drag files
 file_list.drop_target_register(DND_FILES)
