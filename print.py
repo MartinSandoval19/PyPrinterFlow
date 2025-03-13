f57dd0401458fc23089122daa8d1c9dd8ef71dc0
martinsandoval <jsandovala001@uaemex.mx>
2025-03-13 09:48:15 -0600

    Progress bar added to display printing status

--- print.py
+++ print.py
@@ -1,22 +1,12 @@
 import os
 import tkinter
-from tkinter import filedialog
+from tkinter import filedialog, ttk
 from tkinterdnd2 import DND_FILES, TkinterDnD
 import pyautogui
 import time
 from selenium import webdriver
 from selenium.webdriver.edge.service import Service
 from selenium.webdriver.edge.options import Options
-
-web_driver_path = 'edgedriver/msedgedriver.exe'
-
-edgeOptions = Options()
-edgeOptions.add_argument('--headless')
-edgeOptions.add_argument(f'--disable-gpu')
-edgeOptions.add_argument(f'--disable-infobars')
-edgeOptions.add_argument(f'--disable-extensions')
-edgeOptions.add_argument(f'--log-level=3')
-edgeOptions.add_argument('--no-sandox')
 
 # Upload files from select button
 def select_files():
@@ -29,8 +19,20 @@ def drag_files(event):
     for file in files:
         file_list.insert(tkinter.END, file)
 
+# Config webdriver
+web_driver_path = 'edgedriver/msedgedriver.exe'
+edgeOptions = Options()
+edgeOptions.add_argument('--headless')
+edgeOptions.add_argument(f'--disable-gpu')
+edgeOptions.add_argument(f'--disable-infobars')
+edgeOptions.add_argument(f'--disable-extensions')
+edgeOptions.add_argument(f'--log-level=3')
+edgeOptions.add_argument('--no-sandox')
+
 def open_files():
     service = Service(web_driver_path)
+    total_files = file_list.size()
+    progress['maximum'] = total_files
 
     for i in range(file_list.size()):
         file = file_list.get(i)
@@ -45,10 +47,15 @@ def open_files():
 
             pyautogui.press('enter')
             time.sleep(2)
-            print('Se ha impreso el archivo {path}')
+            print(f'Se ha impreso el archivo {path}')
             driver.quit()
         except Exception as e:
             print(f'Error opening file {file}: {e}')
+        
+        progress['value'] = i + 1
+        root.update_idletasks() # Update GUI
+
+    progress['value'] = 0 # Reset progressbar
     
     file_list.delete(0, tkinter.END)
 
@@ -79,6 +86,10 @@ open_button.pack(side=tkinter.RIGHT, padx=10)
 
 remove_button = tkinter.Button(frame_buttons, text="Borrar (0) seleccionados", command=remove_selected_files, width=20)
 remove_button.pack(side=tkinter.LEFT, padx=10)
+
+# Config progressbar
+progress = ttk.Progressbar(root, length=400, mode='determinate')
+progress.pack(pady=10)
 
 # List where the files are displayed
 file_list = tkinter.Listbox(root, width=80, height=15, selectmode=tkinter.MULTIPLE)
